aee9c069c39f083ea74f479e835334ef5e44ba56
root <root@Pc-valentin.localdomain>
2019-12-22 17:17:48 +0100

    fix

--- src/regression.py
+++ src/regression.py
@@ -37,77 +37,80 @@ def loadExplicativeVariable(path,tp):
 		#Longueur moyenne de la chaine de dependance
 		meanDist=getMeanDist(r)
 		xlg.append(meanDist)
-		#xlg.append(np.log(meanDist))
+		xlg.append(np.log(meanDist))
 		names.append("meanDist")
-		#names.append("log meanDist")
+		names.append("log meanDist")
 
 		#Longueur moyenne des phrases
 		meanPhraseLen=mean_phrase_len(r)
 		xlg.append(meanPhraseLen)
-		#xlg.append(np.log(meanPhraseLen))
+		xlg.append(np.log(meanPhraseLen))
 
 		names.append("mean_phrase_len")
-		#names.append("log mean_phrase_len")
+		names.append("log mean_phrase_len")
 
 		#Longueur moyenne des mots
 		MeanWordLength=getMeanWordLength(r)
 		xlg.append(MeanWordLength)
-		#xlg.append(np.log(MeanWordLength))
+		xlg.append(np.log(MeanWordLength))
 
 		names.append("getMeanWordLength")
-		#names.append("log getMeanWordLength")
+		names.append("log getMeanWordLength")
 
 		#Longueur moyenne des lemmes
 		MeanLemmaLength=getMeanLemmaLength(r)
 		xlg.append(MeanLemmaLength)
-		#xlg.append(np.log(MeanLemmaLength))
+		xlg.append(np.log(MeanLemmaLength))
 
 		names.append("getMeanLemmaLength")
-		#names.append("log getMeanLemmaLength")
+		names.append("log getMeanLemmaLength")
 
 		#Nombre de mots uniques utilises
 		wordUsed=nbWordUsed(r)
 		xlg.append(wordUsed)
-		#xlg.append(np.log(wordUsed))
+		xlg.append(np.log(wordUsed))
 
 		names.append("nbWordUsed")
-		#names.append("log nbWordUsed")
+		names.append("log nbWordUsed")
 
 		#Nombre de lemmes uniques utilises
 		lemmaUsed=nbLemmaUsed(r)
 		xlg.append(lemmaUsed)
-		#xlg.append(np.log(lemmaUsed))
+		xlg.append(np.log(lemmaUsed))
 
 		names.append("nbLemmaUsed")
-		#names.append("log nbLemmaUsed")
+		names.append("log nbLemmaUsed")
 
 		#Nombre de caractere uniques utilises
 		charUsed=nbCharUsed(r)
 		xlg.append(charUsed)
-		#xlg.append(np.log(charUsed))
+		xlg.append(np.log(charUsed))
 
 		names.append("nbCharUsed")
-		#names.append("log nbCharUsed")
+		names.append("log nbCharUsed")
 
 		#Mesure de l'ambiguite d'un part of speech
 		#exemple: will peut etre un verbe et un nom
 		POSambiguity=usePOSamb(r)
 		xlg.append(POSambiguity)
-		#xlg.append(np.log(POSambiguity))
+		xlg.append(np.log(POSambiguity))
 
 		names.append("POSambiguity")
+		names.append("log POSambiguity")
 		govLinkLen = getGovernerLinkLength(r)
 		xlg.append(govLinkLen)
-		#xlg.append(np.log(govLinkLen))
+		xlg.append(np.log(govLinkLen))
 
 		names.append("GovLinkLength")
+		names.append("log GovLinkLength")
 		crossGov= getCrossGov(r)
 		xlg.append(crossGov)
-		#xlg.append(np.log(crossGov))
+		xlg.append(np.log(crossGov))
 
 
 		names.append("CrossGov")
-		#names.append("log usePOSamb")
+		names.append("log CrossGov")
+
 
 
 		#Quartiles de la longueur des mots
@@ -115,36 +118,42 @@ def loadExplicativeVariable(path,tp):
 
 		for q in wQuartile:
 			xlg.append(q)
-			#xlg.append(np.log(q))
+			xlg.append(np.log(q))
 
 		names.append("wordQuartile1")
 		names.append("wordQuartile2")
 		names.append("wordQuartile3")
-		#names.append("log wordQuartile")
+		names.append("log wordQuartile1")
+		names.append("log wordQuartile2")
+		names.append("log wordQuartile3")
 
 		#Quartiles de la longueur des lemmes
 		lQuartile=lemmaQuartile(r)
 
 		for q in lQuartile:
 			xlg.append(q)
-			#xlg.append(np.log(q))
+			xlg.append(np.log(q))
 
 		names.append("lQuartile1")
 		names.append("lQuartile2")
 		names.append("lQuartile3")
-		#names.append("log lQuartile")
+		names.append("log lQuartile1")
+		names.append("log lQuartile2")
+		names.append("log lQuartile3")
 
 		#Quartiles de la longueur des phrases
 		sQuartile=sentenceQuartile(r)
 
 		for s in sQuartile:
 			xlg.append(s)
-			#xlg.append(np.log(s))
+			xlg.append(np.log(s))
 
 		names.append("sQuartile1")
 		names.append("sQuartile2")
 		names.append("sQuartile3")
-		#names.append("log sQuartile")
+		names.append("log sQuartile1")
+		names.append("log sQuartile2")
+		names.append("log sQuartile3")
 
 		p=getLanguageProp(r)
 
@@ -165,7 +174,7 @@ def regression(X,y):
 
 	std_scaler = sklearn.preprocessing.StandardScaler()
 	Xreg = std_scaler.fit_transform(X)
-	reg = Lasso(fit_intercept=True,alpha=.5).fit(Xreg, y)
+	reg = Lasso(fit_intercept=True,alpha=0.5).fit(Xreg, y)
 #	reg = LinearRegression().fit(X,y)
 	print("Lasso score r2 {}".format(r2_score(y, reg.predict(Xreg))))
 
@@ -190,12 +199,12 @@ def plotReg(reg,X,y,Y):
 def computeAndSaveFeatures():
 	X,features = X,names=loadExplicativeVariable("../corpus_equilibre","test")
 	np.savetxt("X.in",np.array(X))
-	
+
 	with open("feats.in", 'w') as output:
 		for row in features:
 		    output.write(str(row) + '\n')
 
-	return X,features
+	return X,features,names
 
 def loadFeatures():
 	X=np.loadtxt("X.in")
@@ -206,6 +215,8 @@ def loadFeatures():
 			line = input_feats.readline()
 			features.append(line)
 	return X,features
+
+
 
 
 def main():
@@ -227,9 +238,10 @@ def main():
 	Y_LAS = [y[score][0] for score in y]
 	Y_UAS = [y[score][1] for score in y]
 
-#	X,f = computeAndSaveFeatures()
-	X,names = loadFeatures()
+	X,f,names = computeAndSaveFeatures()
+	#X,names = loadFeatures()
 
+	#X,names=Xlin("../corpus_equilibre/","test")
 	reg = regression(X,Y_LAS)
 	print("Variable explicative beta associe ")
 	print(reg.coef_)
